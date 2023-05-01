be8a1f8fa2f8daeb2f35dc4b66b33eb913072078
jaypee15 <okoyejohnpaul15@gmail.com>
2023-05-01 13:36:42 +0100

    updated user registration page and forms

--- rooms/forms.py
+++ rooms/forms.py
@@ -6,7 +6,7 @@ from .models import Room, User
 class MyUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['name', 'username', 'email', 'password1', 'password2']
+        fields = ['username',  'password1', 'password2']
 
 
 class RoomForm(ModelForm):
--- rooms/models.py
+++ rooms/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
 
 
 class User(AbstractUser):
-    name = models.CharField(max_length=200, null=True)
-    email = models.EmailField(unique=True, null=True)
+    name = models.CharField(max_length=200, null=True, )
+    email = models.EmailField(unique=True, null=True, )
     bio = models.TextField(null=True)
 
     avatar = models.ImageField(null=True, default="avatar.svg")
 
-    USERNAME_FIELD = 'email'
+    # USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+    
 
 
 class Topic(models.Model):
